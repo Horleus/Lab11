cb72ce49484be394983eaa81fc957005d111b3e5
win <win@WINPC>
2020-05-21 23:32:13 +0300

    Ready

--- src/Lab11.cpp
+++ src/Lab11.cpp
@@ -4,8 +4,25 @@ using namespace std;
 
 int main()
 {
-	double f,step,r ;
+	double f,step,a,b,x ;
+	int i,m;
 	setlocale(LC_ALL, "Ukrainian");
+
+	cout << "Уведiть початкове значення вiдрiзку" << endl;
+	cin >> a;
+	cout << "Уведiть кiнцеве значення вiдрiзку" << endl;
+	cin >> b;
+	cout << "Уведiть число розбиття" << endl;
+	cin >> m;
+
+	step = (b-a)/m;
+	x= a;
+	for(i = 0; i< m; i++)
+	{
+		f=1/tan(x) + 1;
+		cout << "Значення функцiї f["<<x<<"]="<<f<<endl;
+		x= x+step;
+	}
 
 	return 0;
 }
